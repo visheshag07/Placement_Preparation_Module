614d389bc8c7ffbaa915939fdb6791389e57ad66
visheshag07 <121476705+visheshag07@users.noreply.github.com>
2023-07-11 18:14:55 +0530

    Time: 12 ms (46.9%), Space: 7.8 MB (27.4%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head,*fast=head;
+        while(slow!=NULL and fast!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+            if(fast)fast=fast->next;
+            if(slow==fast)break;
+        }
+        if(!slow or !fast)return NULL;
+        slow=head;
+        //If you move from head pointer and a pointer from the meeting point it will always meet, a+b=k(b+c)
+
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;
+    }
+};
