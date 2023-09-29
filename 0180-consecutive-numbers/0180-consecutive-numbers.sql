de80b2783ff686dcac4287fdb8b558ad25fdcaf6
visheshag07 <121476705+visheshag07@users.noreply.github.com>
2023-09-30 01:06:02 +0530

    Time: 1026 ms (76.04%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
-with cte as(
-select *, lead(num,1) over() as next_num ,lead(num,2) over() as 
-next_2_num from Logs)
+# with cte as(
+# select *, lead(num,1) over() as next_num ,lead(num,2) over() as 
+# next_2_num from Logs)
 
-select  distinct num as ConsecutiveNums
-from cte where
-num=next_num and num=next_2_num
+# select  distinct num as ConsecutiveNums
+# from cte where
+# num=next_num and num=next_2_num
+
+
+# Write your MySQL query statement below
+select distinct t1.num as ConsecutiveNums
+from logs t1
+join logs t2
+on t1.id=t2.id+1 and t1.num=t2.num
+join logs t3
+on t2.id=t3.id+1 and t2.num=t3.num; 
