cfc8257736063623f4fc70dc2a272a86e52c9dcf
visheshag07 <121476705+visheshag07@users.noreply.github.com>
2023-09-29 23:39:32 +0530

    Time: 1531 ms (98.11%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -13,10 +13,18 @@
 #     GROUP BY product_id
 # )
 
-select product_id,year as first_year ,quantity,price
-from Sales 
-where(product_id,year)in(
-    select product_id ,min(year)
-    from sales
-    group by product_id
-)
+# select product_id,year as first_year ,quantity,price
+# from Sales 
+# where(product_id,year)in(
+#     select product_id ,min(year)
+#     from sales
+#     group by product_id
+# )
+
+
+
+with cte as(select *,rank() over( partition by product_id order by year) as rnk
+from Sales)
+
+select product_id, year as first_year,quantity,price from 
+cte where rnk=1
